17f53bba93657a1e2fd092ebdd6ca14f01a66080
solacens <yippw1025@gmail.com>
2019-04-18 18:24:40 +0800

    another update

--- README.md
+++ README.md
@@ -27,10 +27,16 @@ kubeenv minikube
 # kubeenv [context] [namespace]
 kubeenv minikube some-namespace # But not default
 
+kubeenv list-configs # To list generated seperated configs
+
+kubeenv clean-configs # To clean generated seperated configs incase you changed something in your default KUBECONFIG
+
 # or just using completion 
 kenv <tab><tab> # Namespace autocomplete is still not available
 ```
 
 ### Caveat
+
+- If the tool stucks, it's probably that some of your cluster cannot be connected.
 
 - If you setup your own `KUBECONFIG` environment in `~/.zshrc`, this plugin won't work.
--- kubeenv.plugin.zsh
+++ kubeenv.plugin.zsh
@@ -1,39 +1,56 @@
 kubeenv () {
   # Making kubeenv separated config files
-  mkdir -p $HOME/.kube/kubeenv/
+  BASE_DIR="$HOME/.kube/kubeenv"
+  mkdir -p $BASE_DIR/
+
+  # If its some builtin functions
+  if [ "$1" = "list-configs" ]; then
+    ls -1 $BASE_DIR/
+    return
+  fi
+  if [ "$1" = "clean-configs" ]; then
+    rm -r $BASE_DIR/
+    echo 'Cleaned.'
+    return
+  fi
+
   for context in `kubectl config get-contexts --output='name'`; do
     normalized_context=$(echo $context | sed 's|/|-|g;s|:|-|g;s/|/-/g' )
-    if [ ! -f $HOME/.kube/kubeenv/$normalized_context.yaml ]; then
-      kubectl config view --context $context --minify --raw > $HOME/.kube/kubeenv/$normalized_context.yaml
+    if [ ! -f $BASE_DIR/$normalized_context.yaml ]; then
+      kubectl config view --context $context --minify --raw > $BASE_DIR/$normalized_context.yaml
       for namespace in `kubectl get namespace --output='name' | sed 's|namespace/||'`; do
         if [ "$namespace" != "default" ]; then
-          cat $HOME/.kube/kubeenv/$normalized_context.yaml | yq w - 'contexts.0.context.namespace' "$namespace" > $HOME/.kube/kubeenv/$normalized_context\|$namespace.yaml
+          cat $BASE_DIR/$normalized_context.yaml | yq w - 'contexts.0.context.namespace' "$namespace" > $BASE_DIR/$normalized_context\|$namespace.yaml
         fi
       done
     fi
   done
   # Show usages if no arguments
   if (( $# == 0 )); then
-    echo 'Usage: kubeenv [context]'
+    echo 'Usage:'
+    echo '  kubeenv list-configs'
+    echo '  kubeenv clean-configs'
+    echo '  kubeenv [context]'
+    echo '  kubeenv [context] [namespace]'
     return 1
   fi
 
   if (( $# == 1 )); then
     # If context not found
-    if [ ! -f $HOME/.kube/kubeenv/$1.yaml ]; then
+    if [ ! -f $BASE_DIR/$1.yaml ]; then
       echo "Error: Context not found." >&2
       return 1
     fi
     # Create subshell
-    KUBECONFIG="$HOME/.kube/kubeenv/$1.yaml" $SHELL
+    KUBECONFIG="$BASE_DIR/$1.yaml" $SHELL
   else
     # If context not found
-    if [ ! -f $HOME/.kube/kubeenv/$1\|$2.yaml ]; then
+    if [ ! -f $BASE_DIR/$1\|$2.yaml ]; then
       echo "Error: Context with namespace not found." >&2
       return 1
     fi
     # Create subshell
-    KUBECONFIG="$HOME/.kube/kubeenv/$1|$2.yaml" $SHELL
+    KUBECONFIG="$BASE_DIR/$1|$2.yaml" $SHELL
   fi
 }
 
